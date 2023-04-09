6092af0fe5921e33cf0096c5e8be32aa9676668d
Luis Gabriel Estevão Gabriel <130005206+Lgestevao@users.noreply.github.com>
2023-04-09 21:04:40 +0000

    Exercício_09-Atividade_02

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 try:
-  num_1 = float(input("Digite o preço do primeiro produto: "))
-  num_2 = float(input("Digite o preço do segundo produto: "))
-  num_3 = float(input("Digite o preço do terceiro produto: "))
+  num_1 = float(input("Digite o primeiro número: "))
+  num_2 = float(input("Digite o segundo número: "))
+  num_3 = float(input("Digite o terceiro número: "))
 except:
   print("Digite um número!")
-if num_3 < num_1 > num_2:
+if num_3 > num_1 < num_2:
   if num_3 > num_2:
-    print("Você deve comprar o segundo produto!")
+    print(f"A ordem decrescente é: \n 1- {num_1}; \n 2- {num_2}; \n 3- {num_3}.")
   else:
-    print("Você deve comprar o terceiro produto!")
-elif num_1 < num_2 > num_3:
+    print(f"A ordem decrescente é: \n 1- {num_1}; \n 2- {num_3}; \n 3- {num_2}.")
+elif num_1 > num_2 < num_3:
   if num_1 > num_3:
-    print("Você deve comprar o terceiro produto!")
+    print(f"A ordem decrescente é: \n 1- {num_2}; \n 2- {num_3}; \n 3- {num_1}.")
   else:
-    print("Você deve comprar o primeiro produto!")
+    print(f"A ordem decrescente é: \n 1- {num_2}; \n 2- {num_1}; \n 3- {num_2}.")
 else:
   if num_1 > num_2:
-    print("Você deve comprar o segundo produto!")
+    print(f"A ordem decrescente é: \n 1- {num_3}; \n 2- {num_2}; \n 3- {num_1}.")
   else:
-    print("Você deve comprar o primeiro produto!")
+    print(f"A ordem decrescente é: \n 1- {num_3}; \n 2- {num_1}; \n 3- {num_2}.")
