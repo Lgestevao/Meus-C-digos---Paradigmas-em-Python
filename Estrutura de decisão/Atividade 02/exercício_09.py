b6e7411469c6d1e75c7288f7a93bb6ff80c9589e
Luis Gabriel Estevão Gabriel <130005206+Lgestevao@users.noreply.github.com>
2023-04-09 21:12:43 +0000

    Exercício_10-Atividade_02

--- Estrutura de decisão/Atividade 02/exercício_09.py
+++ Estrutura de decisão/Atividade 02/exercício_09.py
@@ -4,18 +4,18 @@ try:
   num_3 = float(input("Digite o terceiro número: "))
 except:
   print("Digite um número!")
-if num_3 < num_1 > num_2:
+if num_3 > num_1 < num_2:
   if num_3 > num_2:
-    print(f"A ordem decrescente é: \n 1- {num_1}; \n 2- {num_2}; \n {num_3}.")
+    print(f"A ordem decrescente é: \n 1- {num_1}; \n 2- {num_2}; \n 3- {num_3}.")
   else:
-    print(f"A ordem decrescente é: \n 1- {num_1}; \n 2- {num_3}; \n {num_2}.")
-elif num_1 < num_2 > num_3:
+    print(f"A ordem decrescente é: \n 1- {num_1}; \n 2- {num_3}; \n 3- {num_2}.")
+elif num_1 > num_2 < num_3:
   if num_1 > num_3:
-    print(f"A ordem decrescente é: \n 1- {num_2}; \n 2- {num_3}; \n {num_1}.")
+    print(f"A ordem decrescente é: \n 1- {num_2}; \n 2- {num_3}; \n 3- {num_1}.")
   else:
-    print(f"A ordem decrescente é: \n 1- {num_2}; \n 2- {num_1}; \n {num_2}.")
+    print(f"A ordem decrescente é: \n 1- {num_2}; \n 2- {num_1}; \n 3- {num_2}.")
 else:
   if num_1 > num_2:
-    print(f"A ordem decrescente é: \n 1- {num_3}; \n 2- {num_2}; \n {num_1}.")
+    print(f"A ordem decrescente é: \n 1- {num_3}; \n 2- {num_2}; \n 3- {num_1}.")
   else:
-    print(f"A ordem decrescente é: \n 1- {num_3}; \n 2- {num_1}; \n {num_2}.")
+    print(f"A ordem decrescente é: \n 1- {num_3}; \n 2- {num_1}; \n 3- {num_2}.")
