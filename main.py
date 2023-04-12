27752f761a1fe122f5b82ff772216bc9ed83a542
Luis Gabriel Estevão <130005206+Lgestevao@users.noreply.github.com>
2023-04-12 01:36:37 +0000

    Exercício_21-Atividade_02

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 try:
-  nota1 = float(input("Informe a primeira nota: "))
-  nota2 = float(input("Informe a segunda nota: "))
-  nota3 = float(input("Informe a terceira nota: "))
-  
-  if (nota1 > 0 and nota1 <= 10) and (nota2 > 0 and nota2 <=10)  and (nota3 > 0 and nota3 <=10):
-    media = (nota1 + nota2 + nota3) / 3
-    if media >= 7 and media < 10:
-      print("Aprovado!")
-    elif media == 10:
-      print("Aprovado com Distinção!")
-    else:
-      print("Reprovado!")
+  num = int(input("Digite o um número: "))
+  if num % 2 == 0:
+    print("O número é par!")
   else:
-    print("Notas inválidas!")
-except ValueError:
-  print("Só é permitido números. Tente novamente!")
+    print("O número é ímpar!")
+except:
+  print("Digite um número!")
