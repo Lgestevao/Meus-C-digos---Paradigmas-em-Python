61e33e4a15d579d897ab0952ef84f1a6a88c822d
Luis Gabriel Estevão <130005206+Lgestevao@users.noreply.github.com>
2023-04-12 01:04:55 +0000

    Exercício_20-Atividade_02

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 try:
-  num = int(input("Digite um número 1 à 999: "))
-  if num < 0 or num > 999:
-    print("O número deve estar entre 1 e 999. Tente novamente.")
+  nota1 = float(input("Informe a primeira nota: "))
+  nota2 = float(input("Informe a segunda nota: "))
+  nota3 = float(input("Informe a terceira nota: "))
+  
+  if (nota1 > 0 and nota1 <= 10) and (nota2 > 0 and nota2 <=10)  and (nota3 > 0 and nota3 <=10):
+    media = (nota1 + nota2 + nota3) / 3
+    if media >= 7 and media < 10:
+      print("Aprovado!")
+    elif media == 10:
+      print("Aprovado com Distinção!")
+    else:
+      print("Reprovado!")
   else:
-    centena = num/100
-    dezena = num/10
-    unidade = num/1
-    print(f"O número escolhido foi {num:.0f}. \n {num:.0f} = {centena:.0f} centenas, {dezena:.0f} dezenas e {unidade:.0f} unidades.")
-except:
-  print("Tente novamente. Digite um número.")
+    print("Notas inválidas!")
+except ValueError:
+  print("Só é permitido números. Tente novamente!")
