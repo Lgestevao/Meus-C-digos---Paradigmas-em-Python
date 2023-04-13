91689d2ded144f163362ec5fcd1d1392d94b4663
Luis Gabriel Estevão <130005206+Lgestevao@users.noreply.github.com>
2023-04-13 02:07:39 +0000

    Exercício_27-Atividade_02

--- Estrutura de decisão/Atividade 02/exercício_26.py
+++ Estrutura de decisão/Atividade 02/exercício_26.py
@@ -1,14 +1,16 @@
-try:
-  comb = str(input("Escolha qual o tipo de combustível que deseja abastecer digitando: \n \nA - Álcool. \nG - Gasolina. \n \nOpção: "))
-  comb == "A" or comb == "G"
-
-  litros = float(input("\nAgora informe a quantidade de litros desejada: "))
-  litros > 0
-  
-except:
-  print("Digite os dados corretamente e tente novamente.")
-
-try:
-  if comb == "A":
-    if litros > 20:
-      
+litros = float(input("Digite a quantidade de litros que você deseja abastecer: "))
+combustivel = input("Digite A para álcool ou G para gasolina: ")
+preco = 0
+if combustivel == "A" or combustivel == "a":
+    preco = litros * 1.9
+    if litros <= 20:
+        preco -= 1.9 * litros * 3 / 100
+    else:
+        preco -= 1.9 * litros * 5 / 100
+elif combustivel == "G" or combustivel == "g":
+    preco = litros * 2.5
+    if litros <= 20:
+        preco -= 2.5 * litros * 4 / 100
+    else:
+        preco -= 2.5 * litros * 6 / 100
+print(f"O preço a pagar é R${preco:.2f}")
--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-try:
-  print("Respoda as perguntas com SIM ou Não. \n")
-  perg1 = str(input("Telefonou para a vítima? ")).upper()
-  perg2 = str(input("Esteve no local do crime? ")).upper()
-  perg3 = str(input("Mora perto da vítima? ")).upper()
-  perg4 = str(input("Devia para a vítima? ")).upper()
-  perg5 = str(input("Já trabalhou com a vítima? ")).upper()
-
-  soma_perg = 0
-  if perg1 == "SIM":
-    soma_perg += 1
-    
-  if perg2 == "SIM":
-    soma_perg += 1
-    
-  if perg3 == "SIM":
-    soma_perg += 1
-    
-  if perg4 == "SIM":
-    soma_perg += 1
-    
-  if perg5 == "SIM":
-    soma_perg += 1
-    
-  match soma_perg:
-    case 0:
-      print("Inocente!")
-    case 1:
-      print("Inocente!")
-    case 2:
-      print("Supeita!")
-    case 3:
-      print("Cúmplice!")
-    case 4:
-      print("Cúmplice!")
-    case 5:
-      print("Assassino!")
-except:
-  print("Tente novamente respondendo SIM ou Não.")
+litros = float(input("Digite a quantidade de litros que você deseja abastecer: "))
+combustivel = input("Digite A para álcool ou G para gasolina: ")
+preco = 0
+if combustivel == "A" or combustivel == "a":
+    preco = litros * 1.9
+    if litros <= 20:
+        preco -= 1.9 * litros * 3 / 100
+    else:
+        preco -= 1.9 * litros * 5 / 100
+elif combustivel == "G" or combustivel == "g":
+    preco = litros * 2.5
+    if litros <= 20:
+        preco -= 2.5 * litros * 4 / 100
+    else:
+        preco -= 2.5 * litros * 6 / 100
+print(f"O preço a pagar é R${preco:.2f}")
